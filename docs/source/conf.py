4f87cfc1c014e59a6ae12f83d8547e8c24844f34
NicoleChenM <92823883+NicoleChenM@users.noreply.github.com>
2021-11-28 16:13:36 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,7 +31,7 @@ copyright = '2021, RC'
 author = 'RC'
 
 # The full version, including alpha/beta/rc tags
-release = '1.0'
+release = '1.0.0'
 
 
 # -- General configuration ---------------------------------------------------
